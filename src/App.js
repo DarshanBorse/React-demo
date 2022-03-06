a10f43939528b4e9834c0785dc4c27863432538d
DarshanBorse <borsedarshan77@gmail.com>
2022-03-06 22:30:24 +0530

    Add expense data

--- src/App.js
+++ src/App.js
@@ -9,11 +9,16 @@ function App() {
         { id: "2", title: "New TV", amount: "799.49", date: new Date(2022, 2, 12) },
         { id: "3", title: "Car Insurance", amount: "294.12", date: new Date(2022, 2, 1) },
         { id: "4", title: "New Desk", amount: "450", date: new Date(2022, 5, 12) }
-    ]
+    ];
+
+    const addExpenseHandler = (expense) => {
+        console.log('App.js');
+        console.log(expense);
+    }
 
     return (
         <div className='App'>
-            <NewExpense />
+            <NewExpense onAddExpense={addExpenseHandler} />
             <Expence expence={expense} />
         </div>
     );
--- src/components/NewExpense.js
+++ src/components/NewExpense.js
@@ -2,10 +2,18 @@ import React from "react";
 import "../css/NewExpense.css"
 import ExpenseForm from "./ExpenseForm";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+    const saveDataExpenceHandler = (enteredExpenceData) => {
+        const expenseData = {
+            ...enteredExpenceData,
+            id: Math.random().toString(),
+        };
+        props.onAddExpense(expenseData);
+    };
+
     return (
         <div className="new-expense">
-            <ExpenseForm />
+            <ExpenseForm onSaveExpenseData={saveDataExpenceHandler} />
         </div>
     )
 }
